98b634544a1ef18fc3f7408bada8ad2d33d310b1
MuhammadUsmanHasan <42850362+MuhammadUsmanHasan@users.noreply.github.com>
2021-12-13 12:02:50 -0500

    Update weather-observation-station-6.sql

--- Basic Select/weather-observation-station-6.sql
+++ Basic Select/weather-observation-station-6.sql
@@ -5,3 +5,7 @@ CITY LIKE'e%' OR
 CITY LIKE 'i%' OR
 CITY LIKE'o%' OR
 CITY LIKE 'u%';
+
+
+https://www.w3schools.com/sql/func_sqlserver_left.asp
+SELECT DISTINCT CITY FROM STATION WHERE left(City,1) in('a','e','i','o','u');
