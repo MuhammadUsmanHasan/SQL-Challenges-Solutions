58ed6b587149ba99ed580e6c136dec216d5187ad
MuhammadUsmanHasan <42850362+MuhammadUsmanHasan@users.noreply.github.com>
2021-12-15 11:47:52 -0500

    Update Top-Earners.sql

--- Aggretion Challenges/Top-Earners.sql
+++ Aggretion Challenges/Top-Earners.sql
@@ -2,5 +2,5 @@
 /*Uses limit*/
 SELECT SALARY*MONTHS as max_inc, COUNT(*) FROM EMPLOYEE GROUP BY max_inc ORDER BY max_inc desc LIMIT 1;
 
-
-
+/* Without the use of Limit remeber NOTE: Aggregate functions can only be used in GROUP BY and SELECT CLAUSE so another select statenment is need to get the right answer*/
+SELECT MAX(SALARY*MONTHS), COUNT(SALARY*MONTHS)FROM EMPLOYEE WHERE SALARY*MONTHS IN(SELECT MAX(SALARY*MONTHS) FROM EMPLOYEE);
