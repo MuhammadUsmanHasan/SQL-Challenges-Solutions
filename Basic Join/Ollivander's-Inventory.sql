328e6c8edc7778a4ac30f78710994906bb74605d
MuhammadUsmanHasan <42850362+MuhammadUsmanHasan@users.noreply.github.com>
2021-12-30 20:26:06 -0500

    Update Ollivander's-Inventory.sql

--- Basic Join/Ollivander's-Inventory.sql
+++ Basic Join/Ollivander's-Inventory.sql
@@ -1,6 +1,6 @@
 ---https://www.youtube.com/watch?v=756bY8z-B0c&t=2s
 --- this solution does not work on hacker rank server, but works on sqlite
-SELECT w.id, wp.age, w.coins_needed, w.power FROM WAND w JOIN Wands_Property  wp ON 
+SELECT w.id, wp.age, w.coins_needed, w.power FROM Wands w JOIN Wands_Property  wp ON 
 w.code = wp.code
-WHERE id  in (SELECT  id    from Wand Group by code,power HAVING MIN(coins_needed)) AND wp.is_evil!=1
+WHERE id  in (SELECT  id    from Wands Group by code,power HAVING MIN(coins_needed)) AND wp.is_evil!=1
 Order by w.power desc,  age desc;
