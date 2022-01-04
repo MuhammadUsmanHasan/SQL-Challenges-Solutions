4370c5bef8f5d9e899aa6dc2fe51b7642e5b15c7
MuhammadUsmanHasan <42850362+MuhammadUsmanHasan@users.noreply.github.com>
2022-01-03 20:21:41 -0500

    Update Ollivander's-Inventory.sql

--- Basic Join/Ollivander's-Inventory.sql
+++ Basic Join/Ollivander's-Inventory.sql
@@ -1,6 +1,17 @@
----https://www.youtube.com/watch?v=756bY8z-B0c&t=2s
---- this solution does not work on hacker rank server, but works on sqlite
-SELECT w.id, wp.age, w.coins_needed, w.power FROM Wands w JOIN Wands_Property  wp ON 
-w.code = wp.code
-WHERE id  in (SELECT  id    from Wands Group by code,power HAVING MIN(coins_needed)) AND wp.is_evil!=1
-Order by w.power desc,  age desc;
+SELECT w.id, wp.age, w.coins_needed, w.power FROM Wands w JOIN Wands_Property wp ON w.code = wp.code
+WHERE wp.is_evil!=1 AND  w.coins_needed = 
+(SELECT Min(w1.coins_needed) from Wands w1 JOIN Wands_Property wp1 on w1.code = wp1.code 
+ where  w1.power = w.power AND wp1.code = wp.code)
+ORDER BY w.power desc, wp.age desc;
+
+
+
+--Note: w1.power = w.power is needed , it can't be w1.code = w.code because the same code have multiple power 
+-- so in order to get the same age/power we need to use w1.power = w.power
+--Note2: we can swap wp1.code = wp.code because we know what code and age are one-to-one like so:
+
+SELECT w.id, wp.age, w.coins_needed, w.power FROM Wands w JOIN Wands_Property wp ON w.code = wp.code
+WHERE wp.is_evil!=1 AND  w.coins_needed = 
+(SELECT Min(w1.coins_needed) from Wands w1 JOIN Wands_Property wp1 on w1.code = wp1.code 
+ where  w1.power = w.power AND wp1.age = wp.age)
+ORDER BY w.power desc, wp.age desc;
